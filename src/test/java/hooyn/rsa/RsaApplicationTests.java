040875af4ad76581ceca71fd9ad9fcaefc9c13ce
hooyn <hooyn_industry@naver.com>
2022-08-08 08:18:18 +0900

    Initial commit

---
+++ src/test/java/hooyn/rsa/RsaApplicationTests.java
@@ -0,0 +1,13 @@
+package hooyn.rsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
